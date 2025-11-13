6757f91fd7a49bca7d84c143d630164657129a26
Eduard Quiroga <edykiro@gmail.com>
2025-11-13 15:59:52 +0100

    Finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,42 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useApp } from "../contexts/AppCtx";
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const { selectedUser, setSelectedUser } = useApp();
+
+
   return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-  <div className="container-fluid">
-    <a className="navbar-brand fw-bold text-white" href="#">Navbar</a>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+      <div className="container-fluid">
+        <a className="navbar-brand fw-bold text-white" href="#">Navbar</a>
 
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link text-light px-3">Go to Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contacts" className="nav-link text-light px-3">Go to contact list</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/newcontact" className="nav-link text-light px-3">Create new contact</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link text-light px-3">Go to Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contacts" className="nav-link text-light px-3">Go to contact list</Link>
+              </li>
+              <li className="nav-item">
+                <button disabled={!selectedUser} onClick={() => navigate("/newcontact")} className="nav-link px-3">Create new contacto</button>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
